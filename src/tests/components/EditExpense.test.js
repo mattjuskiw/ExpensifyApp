4697d04a9837ddcaa5171a7ebde71fa1733b54aa
Matt Juskiw <juskiwmatt@gmail.com>
2020-04-23 18:02:07 -0400

    Added startEditExpense

--- src/tests/components/EditExpense.test.js
+++ src/tests/components/EditExpense.test.js
@@ -3,14 +3,14 @@ import { shallow } from 'enzyme';
 import { EditExpense } from '../../components/EditExpense';
 import expenses from '../fixtures/expenses';
 
-let editExpense, startRemoveExpense, history, wrapper;
+let startEditExpense, startRemoveExpense, history, wrapper;
 beforeEach(() => {
-	editExpense = jest.fn();
+	startEditExpense = jest.fn();
 	startRemoveExpense = jest.fn();
 	history = { push: jest.fn() };
 	wrapper = shallow(
 		<EditExpense 
-			editExpense={editExpense} 
+		startEditExpense={startEditExpense} 
 			startRemoveExpense={startRemoveExpense} 
 			history={history} 
 			expense={expenses[2]}
@@ -24,10 +24,10 @@ test('should render EditExpense correctly', () => {
 });
 
 // TEST: editExpense()
-test('should handle editExpense', () => {
+test('should handle startEditExpense', () => {
 	wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
 	expect(history.push).toHaveBeenLastCalledWith('/');
-	expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
+	expect(startEditExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
 
 // TEST: startRemoveExpense()
