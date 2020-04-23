e1ac836d38a07d80ad3a35d109593075e376c490
Matt Juskiw <juskiwmatt@gmail.com>
2020-04-23 17:39:34 -0400

    Added loading screen, dispatches redux store to set expenses from Firebase before rendering app

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import visibleExpenses from './selectors/expenses';
 
@@ -22,4 +22,8 @@ const jsx = (
 	</Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+	ReactDOM.render(jsx, document.getElementById('app'));
+});
