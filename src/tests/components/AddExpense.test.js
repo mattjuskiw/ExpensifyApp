aae60e9cfac44d407dfd48c159082dbd6357ed64
Matt Juskiw <juskiwmatt@gmail.com>
2020-04-23 16:55:34 -0400

    Updated component tests for AddExpense with Firebase connection

--- src/tests/components/AddExpense.test.js
+++ src/tests/components/AddExpense.test.js
@@ -3,11 +3,11 @@ import { shallow } from 'enzyme';
 import { AddExpense } from '../../components/AddExpense';
 import expenses from '../fixtures/expenses';
 
-let addExpense, history, wrapper;
+let startAddExpense, history, wrapper;
 beforeEach(() => {
-	addExpense = jest.fn();
+	startAddExpense = jest.fn();
 	history = { push: jest.fn() };
-	wrapper = shallow(<AddExpense addExpense={addExpense} history={history} />);
+	wrapper = shallow(<AddExpense startAddExpense={startAddExpense} history={history} />);
 });
 
 // TEST: Rendering
@@ -19,5 +19,5 @@ test('should render AddExpense correctly', () => {
 test('should handle onSubmit', () => {
 	wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
 	expect(history.push).toHaveBeenLastCalledWith('/');
-	expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
+	expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
