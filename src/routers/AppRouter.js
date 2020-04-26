e16b8fc4fc2f7ad197c2e65450951eb54dd0fc9c
Matt Juskiw <juskiwmatt@gmail.com>
2020-04-26 13:05:27 -0400

    Added PrivateRoute component for AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,25 +3,23 @@ import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
 import Login from '../components/Login';
-import Header from '../components/Header';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import Dashboard from '../components/Dashboard';
 import HelpPage from '../components/HelpPage';
 import NotFound from '../components/NotFound';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
 	<Router history={history}>
 		<div>
-			<Header />
-	
 			<Switch>
 				<Route path="/" component={Login} exact={true} />
-				<Route path="/dashboard" component={Dashboard} />
-				<Route path="/create" component={AddExpense} />
-				<Route path="/edit/:id" component={EditExpense} />
+				<PrivateRoute path="/dashboard" component={Dashboard} />
+				<PrivateRoute path="/create" component={AddExpense} />
+				<PrivateRoute path="/edit/:id" component={EditExpense} />
 				<Route path="/help" component={HelpPage} />
 				<Route component={NotFound} />
 			</Switch>
