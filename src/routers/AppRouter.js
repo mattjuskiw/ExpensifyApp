5128ecf62ac512c83f092a83c2336146a5543ca9
Matt Juskiw <juskiwmatt@gmail.com>
2020-04-26 13:15:19 -0400

    Added PublicRoute component for

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,9 +6,10 @@ import Login from '../components/Login';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import Dashboard from '../components/Dashboard';
-import HelpPage from '../components/HelpPage';
 import NotFound from '../components/NotFound';
+
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -16,11 +17,10 @@ const AppRouter = () => (
 	<Router history={history}>
 		<div>
 			<Switch>
-				<Route path="/" component={Login} exact={true} />
+				<PublicRoute path="/" component={Login} exact={true} />
 				<PrivateRoute path="/dashboard" component={Dashboard} />
 				<PrivateRoute path="/create" component={AddExpense} />
 				<PrivateRoute path="/edit/:id" component={EditExpense} />
-				<Route path="/help" component={HelpPage} />
 				<Route component={NotFound} />
 			</Switch>
 		</div>
