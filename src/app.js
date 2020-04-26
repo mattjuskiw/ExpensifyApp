049373f974a3f5e409a789c6f990624b37dcaeb6
Matt Juskiw <juskiwmatt@gmail.com>
2020-04-26 15:23:16 -0400

    Updated loading screen to new Loading component

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ import 'react-dates/lib/css/_datepicker.css';
 
 import { firebase } from './firebase/firebase';
 
+import Loading from './components/Loading';
+
 const store = configureStore();
 
 const jsx = (
@@ -30,7 +32,7 @@ const renderApp = () => {
 	}
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<Loading />, document.getElementById('app'));
 
 
 firebase.auth().onAuthStateChanged((user) => {
