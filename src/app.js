977ca080276a55f6b909c7858c762a70a18b7ce2
Matt Juskiw <juskiwmatt@gmail.com>
2020-04-26 12:45:04 -0400

    Dispatched login and logout actions based on state on startup

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import visibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
@@ -35,6 +35,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
 	if(user) {
+		store.dispatch(login(user.uid));
 		store.dispatch(startSetExpenses()).then(() => {
 			renderApp();
 			if(history.location.pathname === '/') {
@@ -42,6 +43,7 @@ firebase.auth().onAuthStateChanged((user) => {
 			}
 		});
 	} else {
+		store.dispatch(logout());
 		renderApp();
 		history.push('/');
 	}
